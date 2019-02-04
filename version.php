d86b4d5e8af3270f7d1c1b6de203d4480c1c78e2
Rumano <rumano.balie@getsmarter.com>
2019-02-04 15:19:03 +0200

    More changes to notifications

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_notifications';
 $plugin->release = '1.0.0';
-$plugin->version = 2018120200;
+$plugin->version = 2019014000;
 $plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
